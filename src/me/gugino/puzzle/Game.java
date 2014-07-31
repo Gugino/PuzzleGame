bcb111009a8fd7bd2912b4ea20c61ae05e470050
Joshua Gugino <guginogaming@icloud.com>
2014-07-31 16:39:22 -0400

    Setup java build path, and added unimplemented methods.

--- src/me/gugino/puzzle/Game.java
+++ src/me/gugino/puzzle/Game.java
@@ -1,10 +1,34 @@
 package me.gugino.puzzle;
 
-public class Game {
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+
+	public Game(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	}
 
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
